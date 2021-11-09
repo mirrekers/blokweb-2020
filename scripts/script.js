5a3b7c525b7ddd1fe16ed117fffa3e7e3cbafbef
mirrekers <57792200+mirrekers@users.noreply.github.com>
2021-11-09 18:14:32 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
-var uitgeklapteMenu = document.getElementsByClassName("visible");
 var nav = document.querySelector("nav");
 var menuKnop =document.getElementById("hamburgermenu");
 var closedMenu = document.getElementsByClassName("close")
 
 menuKnop.addEventListener("click",function(){
-  uitgeklapteMenu.classlist.add("visible")
+  nav.classlist.add("visible")
 })
 
 closedMenu.addEventListener("click",function(){
-  uitgeklapteMenu.classlist.remove("visible")
+  nav.classlist.remove("visible")
 })
 
