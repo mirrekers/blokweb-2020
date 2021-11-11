d9a75d5d08ed38318a58cb4cafe91644585afccb
mirrekers <57792200+mirrekers@users.noreply.github.com>
2021-11-11 11:27:26 +0100

    nm

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,39 @@
-var uitgeklapteMenu = document.querySelector("article");
-var menuKnop =document.getElementById("hamburgermenu");
-var closedMenu = document.getElementsByClassName("close")
+// var uitgeklapteMenu = document.querySelector("article");
+// var menuKnop =document.getElementById("hamburgermenu");
+// var closedMenu = document.getElementsByClassName("close")
 
 
-menuKnop.addEventListener("click",function(){
-  uitgeklapteMenu.classlist.add("visible")
-})
+// menuKnop.addEventListener("click",function(){
+//   uitgeklapteMenu.classlist.add("visible")
+// })
 
-closedMenu.addEventListener("click",function(){
-  uitgeklapteMenu.classlist.remove("visible")
-})
+// closedMenu.addEventListener("click",function(){
+//   uitgeklapteMenu.classlist.remove("visible")
+// })
 
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".hamburgermenu");
+
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    // hamburgermenu.style.display = "none";
+    // hamburgermenu.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    // hamburgermenu.style.display = "block";
+    // hamburgermenu.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+// menuItems.forEach( 
+//   function(menuItem) { 
+//     menuItem.addEventListener("click", toggleMenu);
+//   }
+// )
